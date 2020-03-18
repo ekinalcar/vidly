7884ca0795c0934d5328ddb7da8f404b52cf3b88
ekinalcar <ekinalcar@gmail.com>
2020-03-18 12:08:26 +0100

    added rentals

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
