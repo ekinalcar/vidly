8701139a653b082408bbb2053a163e7b731d432f
Ekin Alçar <ekinalcar@gmail.com>
2020-04-01 14:33:25 +0200

    added auth and jwt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const debug = require("debug")("app:startup");
-const mongoose = require("mongoose");
 const config = require("config");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const express = require("express");
 const helmet = require("helmet");
@@ -11,13 +11,20 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.log("FATAL ERROR JWT not provided");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost:27017/vidly", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
   .then(() => {
     console.log("connected to mongodb");
@@ -37,6 +44,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
@@ -8,9 +9,11 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User Already registered.");
+  if (user) return res.status(404).send("User Already registered.");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   return res.send(_.pick(user, ["id", "name", "email"]));
