3bf9cf1a7eed6d662034833f33649b5faf900dc5
Ekin Alçar <ekinalcar@gmail.com>
2020-04-04 11:10:48 +0200

    added middlewares

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("express-async-errors");
 const debug = require("debug")("app:startup");
 const config = require("config");
+const error = require("./middleware/error");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const express = require("express");
@@ -45,6 +47,7 @@ app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,3 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
-
 module.exports = function(req, res, next) {
   if (!req.user.isAdmin) return res.status(403).send("access denied");
   next();
